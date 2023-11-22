177f5b37dde4e29c72c264488a5c42351ebef0b2
vitalname <prostovitalic@gmail.com>
2023-11-22 22:34:50 +0300

    lesson 2.2.2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,9 +1,9 @@
 function factorial(n) {
+    debbugger;
     if (n === 0 || n === 1) {
         return 1;
     } else {
         return n * factorial(n - 1);
-        debbugger;
     }
 }
 console.log(factorial(5));

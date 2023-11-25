26409e3606ff6a749e1efa48f1048aafb3b5ada9
vitalname <prostovitalic@gmail.com>
2023-11-25 07:55:13 +0300

    lesson2.4

---
+++ lesson-02/04.js
@@ -0,0 +1,11 @@
+
+
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if ( value1 === value2 && value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+
+giveMeRightCondition(200, 100, 200)

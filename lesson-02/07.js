dd81ee3a81a927a7dd95b3bd28560a594eeb769b
vitalname <prostovitalic@gmail.com>
2023-11-29 19:03:42 +0300

    lesson2.7

---
+++ lesson-02/07.js
@@ -0,0 +1,17 @@
+
+
+
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried2 = JSON.parse(JSON.stringify(passportMarried))
+passportMarried2.married = true;
+
+console.log(passportMarried);
+console.log(passportMarried2);

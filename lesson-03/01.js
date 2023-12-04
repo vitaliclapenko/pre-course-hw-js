98a0783de7cd398b937bebae47e1c5ed9a0a62d8
vitalname <prostovitalic@gmail.com>
2023-12-04 14:11:25 +0300

    lesson3.1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,12 @@
-// code
+
+
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+
+        let colorGermanu = colors.filter(color => color === "черный" || color === "красный" || color === "желтый")
+        const colorString = colorGermanu.join("-")
+    return colorString;
+}
+
+console.log(createColorString());

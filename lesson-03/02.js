f47ff7325ea4159a70d861bb6ce863ccfccdc7c1
vitalname <prostovitalic@gmail.com>
2023-12-04 14:59:11 +0300

    lesson3.2

---
+++ lesson-03/02.js
@@ -0,0 +1,15 @@
+
+
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+
+
+function shop () {
+    const wordsFruts = words.indexOf("апельсин");
+    if(wordsFruts !== -1){
+        console.log("Ура! нашел");
+    } else {
+        console.log("Придется поискать в другом магазине")
+    }
+}
+
+shop();

c2bf1b973d1572e93405c25ce5a7c7012476c6c4
vitalname <prostovitalic@gmail.com>
2023-12-05 19:38:51 +0300

    lesson 3.3

---
+++ lesson-03/03.js
@@ -0,0 +1,9 @@
+
+
+
+
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+const newFruits = fruits.slice(1,-2);
+
+console.log(newFruits)
